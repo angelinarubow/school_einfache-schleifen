6bfdf2b292c8d55cbcdb19795a8a4ab3096cfdf5
Angelina <brubow@newvisionsproject.de>
2019-03-02 11:44:14 +0100

    initial commit

---
+++ EinfacheSchleifen/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EinfacheSchleifen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // While-Schleifen
+            int number = 1;
+
+            while (number += 5)
+            {
+                Console.WriteLine(number);
+                number += 1;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
